ab7ba006c7e92873aa3c355b0812a35ad3b20b0e
Zutty <george.weller@gmail.com>
2016-05-15 03:45:50 +0100

    Add basic spline implementation and track material. Add terrain from tank vs infantry.

---
+++ Assets/Scripts/CubicBezierSpline.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct CubicBezierSpline {
+
+	public struct Point {
+		public Vector3 position;
+		public Quaternion orientation;
+
+		public Point (Vector3 position, Quaternion orientation) {
+			this.position = position;
+			this.orientation = orientation;
+		}
+
+		public Vector3 LocalToWorld(Vector3 point) {
+			return position + orientation * point;
+		}
+
+		public Vector3 WorldToLocal(Vector3 point) {
+			return Quaternion.Inverse(orientation) * (point - position);
+		}
+
+		public Vector3 LocalToWorldDirection(Vector3 dir) {
+			return orientation * dir;
+		}
+	}
+
+	public Vector3 a;
+	public Vector3 b;
+	public Vector3 c;
+	public Vector3 d;
+
+	public CubicBezierSpline (Vector3 a, Vector3 b, Vector3 c, Vector3 d) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
+	}
+
+	/*
+	Vector3 GetPosition(float t) {
+		float oneMinusT = 1f - t;
+		float oneMinusT2 = oneMinusT * oneMinusT;
+		float t2 = t * t;
+		return a * (oneMinusT2 * oneMinusT)
+		+ b * (3f * oneMinusT2 * t)
+		+ c * (3f * oneMinusT * t2)
+		+ d * (t2 * t);
+	}
+
+	Vector3 GetTangent(float t) {
+		float oneMinusT = 1f - t;
+		float oneMinusT2 = oneMinusT * oneMinusT;
+		float t2 = t * t;
+		return (a * (-oneMinusT2)
+		+ b * (3f * oneMinusT2 - 2f * oneMinusT)
+		+ c * (-3f * t2 + 2f * t)
+		+ d * (t2)).normalized;
+	}
+
+	Vector3 GetNormal(float t, Vector3 up) {
+		Vector3 tangent = GetTangent(t);
+		Vector3 binormal = Vector3.Cross(up, tangent).normalized;
+		return Vector3.Cross(tangent, binormal);
+	}
+
+	Quaternion GetOrientation(float t, Vector3 up) {
+		Vector3 tng = GetTangent(t);
+		Vector3 nrm = GetNormal(t, up);
+		return Quaternion.LookRotation(tng, nrm);
+	}
+*/
+
+	public Point GetPoint(float t) {
+		float oneMinusT = 1f - t;
+		float oneMinusT2 = oneMinusT * oneMinusT;
+		float t2 = t * t;
+
+		Vector3 position = a * (oneMinusT2 * oneMinusT)
+		                   + b * (3f * oneMinusT2 * t)
+		                   + c * (3f * oneMinusT * t2)
+		                   + d * (t2 * t);
+
+		Vector3 tangent = a * (-oneMinusT2)
+		                  + b * (3f * oneMinusT2 - 2f * oneMinusT)
+		                  + c * (-3f * t2 + 2f * t)
+		                  + d * (t2);
+
+		Vector3 binormal = Vector3.Cross(Vector3.up, tangent).normalized;
+		Vector3 normal = Vector3.Cross(tangent, binormal);
+		Quaternion orientation = Quaternion.LookRotation(tangent, normal);
+
+		return new Point (position, orientation);
+	}
+
+	public void Sample(Point[] points) {
+		float len = points.Length - 1;
+		for (int n = 0; n <= len; n++) {
+			float t = (float)n / len;
+			points [n] = GetPoint(t);
+		}
+	}
+}
---
+++ Assets/Scripts/Extrude.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Extrude : MonoBehaviour {
+
+	[System.Serializable]
+	public struct Shape {
+		public Vector2[] vertices;
+		public Vector2[] normals;
+		public float[] u;
+		public int[] lines;
+	}
+
+	public Shape shape;
+	public Transform a;
+	public Transform b;
+	public Transform c;
+	public Transform d;
+
+	private CubicBezierSpline spline;
+	private Mesh mesh;
+
+	private CubicBezierSpline.Point[] splinePoints;
+	private int[] triangles;
+	private Vector3[] vertices;
+	private Vector3[] normals;
+	private Vector2[] uv;
+
+	void Start() {
+		mesh = GetComponent<MeshFilter>().sharedMesh = new Mesh ();
+		spline = new CubicBezierSpline (a.position, b.position, c.position, d.position);
+		Resize(16);
+	}
+
+	void Update() {
+		Recalculate();
+	}
+
+	void Resize(int splineLen) {
+		splinePoints = new CubicBezierSpline.Point[splineLen];
+
+		Debug.Log(splinePoints);
+
+		int vertexCount = shape.vertices.Length * splineLen;
+		int indexCount = shape.lines.Length * (splineLen - 1) * 3;
+
+		triangles = new int[ indexCount ];
+		vertices = new Vector3[ vertexCount ];
+		normals = new Vector3[ vertexCount ];
+		uv = new Vector2[ vertexCount ];
+
+		Recalculate();
+	}
+
+	void Recalculate() {
+		spline.a = a.position;
+		spline.b = b.position;
+		spline.c = c.position;
+		spline.d = d.position;
+		spline.Sample(splinePoints);
+
+		int splineLen = splinePoints.Length;
+		int shapeVertices = shape.vertices.Length;
+
+		for (int i = 0; i < splineLen; i++) {
+			int offset = i * shapeVertices;
+			for (int j = 0; j < shapeVertices; j++) {
+				int id = offset + j;
+				vertices [id] = splinePoints [i].LocalToWorld(shape.vertices [j]);
+				normals [id] = splinePoints [i].LocalToWorldDirection(shape.normals [j]);
+				uv [id] = new Vector2 (shape.u [j], i / (float)splineLen);
+			}
+		}
+
+		int ti = 0;
+		for (int i = 0; i < splineLen - 1; i++) {
+			int offset = i * shapeVertices;
+			for (int l = 0; l < shape.lines.Length; l += 2) {
+				int pa = offset + shape.lines [l] + shapeVertices;
+				int pb = offset + shape.lines [l];
+				int pc = offset + shape.lines [l + 1];
+				int pd = offset + shape.lines [l + 1] + shapeVertices;
+				triangles[ti] = pa; 	ti++;
+				triangles[ti] = pb; 	ti++;
+				triangles[ti] = pc; 	ti++;
+				triangles[ti] = pc; 	ti++;
+				triangles[ti] = pd; 	ti++;
+				triangles[ti] = pa; 	ti++;
+			}
+		}
+
+		mesh.Clear();
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		mesh.normals = normals;
+		mesh.uv = uv;
+	}
+}
