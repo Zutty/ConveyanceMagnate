928c41e4432fc4fe3b4a639097704f52f90219ab
Zutty <george.weller@gmail.com>
2016-05-23 00:36:12 +0100

    Refactor spline code to use coroutine and reformat a bit.

--- Assets/Scripts/Extrude.cs
+++ Assets/Scripts/Extrude.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class Extrude : MonoBehaviour {
 
@@ -20,14 +20,14 @@ public class Extrude : MonoBehaviour {
 	private CatmullRomSpline spline;
 	private Mesh mesh;
 
-	private CatmullRomSpline.Point[] splinePoints;
+	private int splineLen;
 	private int[] triangles;
 	private Vector3[] vertices;
 	private Vector3[] normals;
 	private Vector2[] uv;
 
 	void Start() {
-		mesh = GetComponent<MeshFilter>().sharedMesh = new Mesh ();
+		mesh = GetComponent<MeshFilter>().sharedMesh = new Mesh();
 		//spline = new CubicBezierSpline (a.position, b.position, c.position, d.position);
 		Resize(16);
 	}
@@ -37,10 +37,7 @@ public class Extrude : MonoBehaviour {
 	}
 
 	void Resize(int splineLen) {
-		splinePoints = new CatmullRomSpline.Point[splineLen];
-
-		Debug.Log(splinePoints);
-
+		this.splineLen = splineLen;
 		int vertexCount = shape.vertices.Length * splineLen;
 		int indexCount = shape.lines.Length * (splineLen - 1) * 3;
 
@@ -57,40 +54,36 @@ public class Extrude : MonoBehaviour {
 		spline.p1 = b.position;
 		spline.p2 = c.position;
 		spline.p3 = d.position;
-		spline.Sample(splinePoints);
 
-		int splineLen = splinePoints.Length;
 		int shapeVertices = shape.vertices.Length;
 
-		for (int i = 0; i < splineLen; i++) {
-			int offset = i * shapeVertices;
-			for (int j = 0; j < shapeVertices; j++) {
-				int id = offset + j;
-				vertices [id] = transform.InverseTransformPoint(splinePoints [i].LocalToWorld(shape.vertices [j]));
-				normals [id] = splinePoints [i].LocalToWorldDirection(shape.normals [j]);
-				uv [id] = new Vector2 (shape.u [j], i / (float)splineLen);
+		int idx = 0;
+		foreach(CatmullRomSpline.Point p in spline.Sample(splineLen)) {
+			for(int j = 0; j < shapeVertices; j++, idx++) {
+				vertices[idx] = transform.InverseTransformPoint(p.LocalToWorld(shape.vertices[j]));
+				normals[idx] = p.LocalToWorldDirection(shape.normals[j]);
+				uv[idx] = new Vector2(shape.u[j], p.index / (float)splineLen);
 			}
 		}
 
 		int ti = 0;
-		for (int i = 0; i < splineLen - 1; i++) {
+		for(int i = 0; i < splineLen - 1; i++) {
 			int offset = i * shapeVertices;
-			for (int l = 0; l < shape.lines.Length; l += 2) {
-				int pa = offset + shape.lines [l] + shapeVertices;
-				int pb = offset + shape.lines [l];
-				int pc = offset + shape.lines [l + 1];
-				int pd = offset + shape.lines [l + 1] + shapeVertices;
-				triangles[ti] = pa; 	ti++;
-				triangles[ti] = pb; 	ti++;
-				triangles[ti] = pc; 	ti++;
-				triangles[ti] = pc; 	ti++;
-				triangles[ti] = pd; 	ti++;
-				triangles[ti] = pa; 	ti++;
+			for(int l = 0; l < shape.lines.Length; l += 2) {
+				int pa = offset + shape.lines[l] + shapeVertices;
+				int pb = offset + shape.lines[l];
+				int pc = offset + shape.lines[l + 1];
+				int pd = offset + shape.lines[l + 1] + shapeVertices;
+				triangles[ti++] = pa;
+				triangles[ti++] = pb;
+				triangles[ti++] = pc;
+				triangles[ti++] = pc;
+				triangles[ti++] = pd;
+				triangles[ti++] = pa;
 			}
 		}
 
 		mesh.Clear();
-		//mesh.
 		mesh.vertices = vertices;
 		mesh.triangles = triangles;
 		mesh.normals = normals;
--- Assets/Scripts/SplineView.cs
+++ Assets/Scripts/SplineView.cs
@@ -25,20 +25,20 @@ public class SplineView : MonoBehaviour {
 
 		if(controlPoints.Length == 4) {
 			float t = 0;
-			Vector3 p = crSpline.GetCentripetalCR(controlPoints[0].position, controlPoints[1].position, controlPoints[2].position, controlPoints[3].position, t);
+			Vector3 p;// = crSpline.GetCentripetalCR(controlPoints[0].position, controlPoints[1].position, controlPoints[2].position, controlPoints[3].position, t);
 			Vector3 q, tangent;
 			for(int i = 0; i <= 10; i++) {
-				q = crSpline.GetCentripetalCR(controlPoints[0].position, controlPoints[1].position, controlPoints[2].position, controlPoints[3].position, t);
-				tangent = crSpline.GetCentripetalCRTangent(controlPoints [0].position, controlPoints [1].position, controlPoints [2].position, controlPoints [3].position, t).normalized;
+				//q = crSpline.GetCentripetalCR(controlPoints[0].position, controlPoints[1].position, controlPoints[2].position, controlPoints[3].position, t);
+				//tangent = crSpline.GetCentripetalCRTangent(controlPoints [0].position, controlPoints [1].position, controlPoints [2].position, controlPoints [3].position, t).normalized;
 
 				Gizmos.color = Color.yellow;
-				Gizmos.DrawRay(new Ray (q, tangent));
+				//Gizmos.DrawRay(new Ray (q, tangent));
 
 				Gizmos.color = Color.white;
-				Gizmos.DrawLine(p, q);
+				//Gizmos.DrawLine(p, q);
 
 				t += 0.1f;
-				p = q;
+				//p = q;
 			}
 		}
 	}
