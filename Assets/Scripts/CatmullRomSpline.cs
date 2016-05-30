c3de0068b6a157bbeb1bb23e8ebc4aa3b0fc9ab3
Zutty <george.weller@gmail.com>
2016-05-30 16:09:22 +0100

    Parameterize by arc length

--- Assets/Scripts/CatmullRomSpline.cs
+++ Assets/Scripts/CatmullRomSpline.cs
@@ -26,6 +26,78 @@ public struct CatmullRomSpline {
 	public Vector3 p2;
 	public Vector3 p3;
 
+	public float Speed(float t) {
+		float dt0 = Mathf.Sqrt(Vector3.Distance(p0, p1));
+		float dt1 = Mathf.Sqrt(Vector3.Distance(p1, p2));
+		float dt2 = Mathf.Sqrt(Vector3.Distance(p2, p3));
+
+		// safety check for repeated points
+		if(dt1 < Mathf.Epsilon) {
+			dt1 = 1.0f;
+		}
+		if(dt0 < Mathf.Epsilon) {
+			dt0 = dt1;
+		}
+		if(dt2 < Mathf.Epsilon) {
+			dt2 = dt1;
+		}
+
+		Vector3 m1 = (p1 - p0) / dt0 - (p2 - p0) / (dt0 + dt1) + (p2 - p1) / dt1;
+		Vector3 m2 = (p2 - p1) / dt1 - (p3 - p1) / (dt1 + dt2) + (p3 - p2) / dt2;
+
+		m1 *= dt1;
+		m2 *= dt1;
+
+		Vector3 d0 = m1;
+		Vector3 d1 = -6 * p1 + 6 * p2 - 4 * m1 - 2 * m2;
+		Vector3 d2 = 6 * p1 - 6 * p2 + 3 * m1 + 3 * m2;
+
+		Vector3 tangent = d0 + d1 * t + d2 * t * t;
+		return tangent.magnitude;
+	}
+
+	public float ArcLength(float tmax) {
+		float dt0 = Mathf.Sqrt(Vector3.Distance(p0, p1));
+		float dt1 = Mathf.Sqrt(Vector3.Distance(p1, p2));
+		float dt2 = Mathf.Sqrt(Vector3.Distance(p2, p3));
+
+		// safety check for repeated points
+		if(dt1 < Mathf.Epsilon) {
+			dt1 = 1.0f;
+		}
+		if(dt0 < Mathf.Epsilon) {
+			dt0 = dt1;
+		}
+		if(dt2 < Mathf.Epsilon) {
+			dt2 = dt1;
+		}
+
+		Vector3 m1 = (p1 - p0) / dt0 - (p2 - p0) / (dt0 + dt1) + (p2 - p1) / dt1;
+		Vector3 m2 = (p2 - p1) / dt1 - (p3 - p1) / (dt1 + dt2) + (p3 - p2) / dt2;
+
+		m1 *= dt1;
+		m2 *= dt1;
+
+		Vector3 d0 = m1;
+		Vector3 d1 = -6 * p1 + 6 * p2 - 4 * m1 - 2 * m2;
+		Vector3 d2 = 6 * p1 - 6 * p2 + 3 * m1 + 3 * m2;
+
+		float prev = d0.magnitude;
+
+		float sum = 0f;
+		for(int i = 1; i <= 10; i++) {
+			float t = (i / 10f) * tmax;
+
+			Vector3 tangent = d0 + d1 * t + d2 * t * t;
+
+			sum += (t - (((i - 1) / 10f) * tmax)) * (prev + tangent.magnitude);
+
+			prev = tangent.magnitude;
+		}
+
+		return  sum / 2f;
+	}
+
 	public Point GetPoint(float t) {
 		float dt0 = Mathf.Sqrt(Vector3.Distance(p0, p1));
 		float dt1 = Mathf.Sqrt(Vector3.Distance(p1, p2));
@@ -73,7 +145,32 @@ public struct CatmullRomSpline {
 		return p;
 	}
 
+	public float GetCurveParameter(float s) {
+		float t = 0;
+		int n = 10;
+		float h = s/n;
+		for (int i = 1; i <= n; i++) {
+			float k1 = h/Speed(t);
+			float k2 = h/Speed(t + k1/2);
+			float k3 = h/Speed(t + k2/2);
+			float k4 = h/Speed(t + k3);
+			t += (k1 + 2 * (k2 + k3) + k4)/6;
+		}
+		return t ; 
+	}
+
 	public IEnumerable<Point> Sample(int len) {
+		float l = ArcLength(1);
+		for(int n = 0; n < len; n++) {
+			float s = (float)(n / (len - 1f)) * l;
+			Point p = GetPoint(GetCurveParameter(s));
+			p.index = n;
+			yield return p;
+		}
+	}
+
+
+	public IEnumerable<Point> SampleUnparameterized(int len) {
 		for(int n = 0; n < len; n++) {
 			float t = (float)n / (len - 1);
 			Point p = GetPoint(t);
