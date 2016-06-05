e0e1625c173904f0ae14c9568627e8de98685508
Zutty <george.weller@gmail.com>
2016-06-05 16:13:26 +0100

    Add hacky starting point for a GUI

---
+++ Assets/Scripts/UIStateManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIStateManager : MonoBehaviour {
+
+	public enum UIState { NORMAL, CREATE_TRACK }
+
+	private static UIStateManager _instance;
+
+	private UIState _state = UIState.NORMAL;
+
+	void Start () {
+		_instance = this;
+	}
+	
+	void Update () {
+	
+	}
+
+	public static UIStateManager instance {
+		get { return _instance; }
+	}
+
+	public UIState state {
+		get { return _state; }
+	}
+
+	public void ClearState() {
+		_state = UIState.NORMAL;
+	}
+
+	public void TriggerCreateTrack() {
+		_state = UIState.CREATE_TRACK;
+	}
+}
