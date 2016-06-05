c1d98d8cdabef2a83b9083d158e22188cf51d0a1
Zutty <george.weller@gmail.com>
2016-06-05 16:12:42 +0100

    Fix handle highlighting and prefabs

--- Assets/Scripts/Handle.cs
+++ Assets/Scripts/Handle.cs
@@ -5,31 +5,45 @@ public class Handle : MonoBehaviour {
 
 	public static readonly Color PURPLE = new Color(229, 29, 137);
 
+	public enum HandleMode { DRAG, MOVE }
+
+	public HandleMode mode = HandleMode.DRAG;
+	public Material selectMaterial;
 	public LayerMask layerMask;
 
 	private Projector _projector;
+	private Material _baseMaterial;
 
 	void Start () {
 		_projector = GetComponent<Projector> ();
+		_baseMaterial = _projector.material;
 	}
 	
 	void Update () {
-		
-	}
-
-	void OnMouseOver() {
-		//_projector.material.SetColor("_Color", Color.blue);
-	}
-
-	void OnMouseExit() {
-		//_projector.material.SetColor("_Color", PURPLE);
+		if(mode == HandleMode.MOVE) {
+			Reposition();
+		}
 	}
 
 	void OnMouseDrag() {
+		if(mode == HandleMode.DRAG) {
+			Reposition();
+		}
+	}
+
+	private void Reposition() {
 		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 		RaycastHit hit;
 		if (Physics.Raycast (ray, out hit, 1000, layerMask) && hit.collider.gameObject.CompareTag("terrain")) {
 			transform.parent.position = hit.point;
 		}
+	}
+
+	void OnMouseOver() {
+		_projector.material = selectMaterial;
+	}
+
+	void OnMouseExit() {
+		_projector.material = _baseMaterial;
 	}
 }
