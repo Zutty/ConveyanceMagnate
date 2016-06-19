dee31cc3211bf8f1aa1e980ec03132bb3e2a673f
Zutty <george.weller@gmail.com>
2016-06-19 11:21:59 +0100

    Spline follower move on input and respect vehicle length

--- Assets/Scripts/SplineFollower.cs
+++ Assets/Scripts/SplineFollower.cs
@@ -9,10 +9,27 @@ public class SplineFollower : MonoBehaviour {
 	public Transform frontFollower;
 	public Transform rearFollower;
 	public float baseLength;
+	public float bufferLength = 2f;
 
 	private float distance = 0f;
+	private bool _move = false;
 
-	void Update () {
+	public void Start() {
+		distance = baseLength + bufferLength;
+		Move();
+	}
+
+	public void Update () {
+		if(Input.GetKey(KeyCode.M)) {
+			_move = true;
+			distance = baseLength + bufferLength;
+		}
+		if(_move) {
+			Move();
+		}
+	}
+
+	public void Move() {
 		distance += speed * Time.deltaTime;
 
 		SplinePoint front = spline.GetPoint(distance);
