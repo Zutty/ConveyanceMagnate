973046314eb6e1bf3aefd920395d1cf945eff729
Zutty <george.weller@gmail.com>
2016-06-27 14:04:38 +0100

    WIP rather broken and buggy junction implementation

--- Assets/Scripts/UIStateManager.cs
+++ Assets/Scripts/UIStateManager.cs
@@ -14,8 +14,10 @@ public class UIStateManager : MonoBehaviour {
 
 	public GameObject createTrackHandle;
 	public GameObject trackPrefab;
-	public LayerMask layerMask;
 	public SplineFollower trainPrefab;
+
+	public LayerMask groundLayerMask;
+	public LayerMask uiLayerMask;
 
 	private UIState _state = UIState.NORMAL;
 	private Transform _draggable;
@@ -29,22 +31,37 @@ public class UIStateManager : MonoBehaviour {
 	
 	void Update () {
 		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-		RaycastHit hit;
 		Vector3 mousePosition = Vector3.zero;
-		if (Physics.Raycast(ray, out hit, 1000, layerMask) && hit.collider.gameObject.CompareTag("terrain")) {
-			mousePosition = hit.point;
+
+		RaycastHit groundHit;
+		if (Physics.Raycast(ray, out groundHit, 1000, groundLayerMask) && groundHit.collider.gameObject.CompareTag("terrain")) {
+			mousePosition = groundHit.point;
 		}
+
+		RaycastHit uiHit;
+		bool isUiHit = Physics.Raycast(ray, out uiHit, 1000, uiLayerMask) && uiHit.collider.gameObject.CompareTag("SplineHandle");
 
 		// TODO Split this bullshit into separate classes you goit
 		if(_state == UIState.CREATE_TRACK) {
+			_trackHandleProjector.enabled = !isUiHit;
+
 			createTrackHandle.transform.position = mousePosition;
 				
 			if(Input.GetMouseButtonDown(0)) {
 				_state = UIState.DRAG_TRACK;
 				_trackHandleProjector.enabled = false;
+
 				GameObject newTrack = (GameObject)Instantiate(trackPrefab);
 				CompositeSpline spline = newTrack.GetComponent<CompositeSpline>();
-				spline.points[1].position = mousePosition;
+
+				if(isUiHit) {
+					// Reuse existing point
+					spline.points[1] = uiHit.collider.gameObject.transform;
+				} else {
+					// Create new point
+					spline.points[1].position = mousePosition;
+				}
+
 				_draggable = spline.points[2];
 				_draggable.position = mousePosition;
 			}
@@ -74,6 +91,10 @@ public class UIStateManager : MonoBehaviour {
 	public void ClearState() {
 		_state = UIState.NORMAL;
 		_draggable = null;
+	}
+
+	public bool IsInState(UIState state) {
+		return _state == state;
 	}
 
 	public void TriggerCreateTrack() {
